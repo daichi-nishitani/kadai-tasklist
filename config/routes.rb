9481d75002dd2cab7104de5104abefc02289fae4
daichi.nishitani <d.photography.0518@gmail.com>
2020-03-15 14:52:15 +0000

    toppage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get 'toppages/index'
+  root to: 'toppages#index'
+  
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'sessions/new'
+  get 'sessions/ctreate'
+  get 'sessions/destroy'
+  
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
   
   resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
 end
