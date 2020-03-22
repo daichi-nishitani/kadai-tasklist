a342f11bf15eccd217628fea22070412f1631314
daichi.nishitani <d.photography.0518@gmail.com>
2020-03-22 14:14:59 +0000

    correct_user

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   get 'toppages/index'
   root to: 'tasks#index'
   
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
   get 'sessions/new'
   get 'sessions/ctreate'
   get 'sessions/destroy'
